241b0f1bd08d0efd994201b343bea1c7946729c9
Thuan Dang <thuandang@Thuans-Mac-mini.local>
2021-09-08 16:16:50 -0500

    make changes

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Trainers from './Trainers'
 
-class Categories extends React.Component {
 
-    render() {
+// bodybuilding, running, power_lifting, cycling, swimming
+const Categories = ({trainers}) => {
+    const [bodyBuilding, setBodyBuilding] = useState(false)
+    const [running, setRunning] = useState(false)
+    const [powerLifting, setPowerLifting] = useState(false)
+    const [cycling, setCycling] = useState(false)
+    const [swimming, setSwimming] = useState(false)
 
-        const {trainers} = this.props
 
-        return (
-            <div className='container'>
-                <h2>{trainers}</h2>
-
-                
-            </div>
-        )
+    const handleClick = () => {
+        setBodyBuilding(true)
+        console.log(bodyBuilding)
     }
+
+
+    return (
+        <div>
+            <h2 onClick={handleClick}>Body Building</h2>
+            <h2>Running</h2>
+            <h2>Power Lifting</h2>
+            <h2>Cycling</h2>
+            <h2>Swimming</h2>
+            {bodyBuilding && <Trainers trainers={trainers}/>}
+        </div>
+    )
 }
+
 
 export default Categories
