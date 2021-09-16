25f6c303cb56977f028767688a09a7bc28579546
Alfred <85899437+abenford09@users.noreply.github.com>
2021-09-16 14:57:10 -0500

    Alfred

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const cors = require('cors')
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'thuandang',
-    // password: 'password',
+    user: 'root',
+    password: 'password',
     host: 'localhost',
-    // port: 5434,
+    port: 5433,
     database: 'fce'
 })
 
